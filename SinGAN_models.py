d75ac8a4e3b8ad5ca0aab06e624899af807a531e
skywolf829 <swwurster@gmail.com>
2020-10-09 23:30:16 -0400

    Update SinGAN_models.py

--- SinGAN_models.py
+++ SinGAN_models.py
@@ -410,7 +410,7 @@ def train_single_scale(generators, discriminators, opt):
     optimal_LR = torch.zeros(generator.get_input_shape(), device=opt["device"])
     opt["noise_amplitudes"].append(1.0)
     if(len(generators) > 0):
-        with(torch.no_grad):
+        with torch.no_grad:
             optimal_LR = generate(generators, "reconstruct", opt, opt["device"])
         optimal_LR = F.interpolate(optimal_LR, size=opt["resolutions"][len(generators)],
         mode=opt["upsample_mode"])
