ed0943d0551b9d62b1a6521bc05e55e8a5daf64d
skywolf829 <swwurster@gmail.com>
2020-10-13 20:54:36 -0400

    Update options.py

--- options.py
+++ options.py
@@ -19,7 +19,7 @@ class Options():
         
         # GAN info
         opt["num_blocks"]              = 5
-        opt["base_num_kernels"]        = 16        # Num of kernels in smallest scale conv layers
+        opt["base_num_kernels"]        = 32        # Num of kernels in smallest scale conv layers
         opt["pre_padding"]             = False         # Padding on conv layers in the GAN
         opt["kernel_size"]             = 3
         opt["stride"]                  = 1
