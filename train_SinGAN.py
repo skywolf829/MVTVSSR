b0aaf2f7f081dc60ae49039925744a7b1d0294eb
skywolf829 <swwurster@gmail.com>
2020-10-16 15:02:56 -0400

    Update train_SinGAN.py

--- train_SinGAN.py
+++ train_SinGAN.py
@@ -54,6 +54,7 @@ if __name__ == '__main__':
     parser.add_argument('--gamma',default=None, type=float,help='')
     parser.add_argument('--physical_constraints',default=None,type=str,help='none, soft, or hard')
     parser.add_argument('--patch_size',default=None, type=int,help='Patch size for inference')
+    parser.add_argument('--training_patch_size',default=None, type=int,help='Patch size for training')
     parser.add_argument('--upsample_mode',default=None, type=str,help='Method for upsampling')
 
     parser.add_argument('--load_from',default=None, type=str,help='Load a model to continue training')
