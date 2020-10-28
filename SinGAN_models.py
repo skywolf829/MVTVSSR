555460f5516052aeaa9f4676e4f111a71192a4ce
Skylar Wurster <swwurster@gmail.com>
2020-10-28 13:19:13 -0400

    Update SinGAN_models.py

--- SinGAN_models.py
+++ SinGAN_models.py
@@ -652,6 +652,7 @@ def train_single_scale(generators, discriminators, opt):
     for epoch in range(opt["epochs"]):
 
         for patch_in_training in range(len(starts_all)):
+            print(patch_in_training)
             D_loss = 0
             G_loss = 0        
             gradient_loss = 0
