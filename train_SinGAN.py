4bbcff3a5ef8837d12a55e1333d7a62f163570a9
Skylar Wurster <swwurster@gmail.com>
2020-10-29 10:22:48 -0400

    Update train_SinGAN.py

--- train_SinGAN.py
+++ train_SinGAN.py
@@ -90,6 +90,9 @@ if __name__ == '__main__':
         opt = load_options(os.path.join(save_folder, args["load_from"]))
         opt["device"] = args["device"]
         opt["save_name"] = args["load_from"]
+        for k in args.keys():
+            if args[k] is not None:
+                opt[k] = args[k]
         generators, discriminators = load_models(opt,args["device"])
 
     
