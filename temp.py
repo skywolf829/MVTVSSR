2bcb317645fdc2be3fca49f8464decd2ca369f47
Skylar Wurster <swwurster@gmail.com>
2020-11-15 19:28:31 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -19,37 +19,8 @@ input_folder = os.path.join(MVTVSSR_folder_path, "InputData")
 output_folder = os.path.join(MVTVSSR_folder_path, "Output")
 save_folder = os.path.join(MVTVSSR_folder_path, "SavedModels")
 
-parser = argparse.ArgumentParser(description='Test a trained model')
 
-parser.add_argument('--load_from',default="128_GP_0.5")
-parser.add_argument('--data_folder',default="JHUturbulence/isotropic128_3D",type=str,help='File to test on')
-parser.add_argument('--device',default="cuda:0",type=str,help='Frames to use from training file')
-
-args = vars(parser.parse_args())
-
-opt = load_options(os.path.join(save_folder, args["load_from"]))
-opt["device"] = args["device"]
-opt["save_name"] = args["load_from"]
-
-
-
-generators, discriminators = load_models(opt,args["device"])
-
-for i in range(len(generators)):
-    generators[i] = generators[i].to(args["device"])
-    generators[i] = generators[i].eval()
-for i in range(len(discriminators)):
-    discriminators[i].to(args["device"])
-    discriminators[i].eval()
-
-
-dataset = Dataset(os.path.join(input_folder, args["data_folder"]), opt)
-a = dataset.__getitem__(0).cuda()
-
-
-paths = lagrangian_transport3D(torch.ones([1, 3, 100, 100, 100]).cuda(),
-1, 1, 1, 1, 1, "cuda")
-paths2 = lagrangian_transport3D(torch.ones([1, 3, 100, 100, 100]).cuda()*2,
-1, 1, 1, 1, 1, "cuda")
-
-print(pathline_distance(paths, paths2))
+a = np.load("0.npy")
+a = np2torch(a, "cuda:1").unsqueeze(0)
+a = laplace_pyramid_downscale3D(a, 2, 0.5, "cuda:1")
+print(a.shape)
