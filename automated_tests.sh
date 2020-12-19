ddf3f981eab26cdd2768b90431d815414b39492c
Skylar Wurster <swwurster@gmail.com>
2020-12-19 00:06:48 -0500

    Update automated_tests.sh

--- automated_tests.sh
+++ automated_tests.sh
@@ -1,47 +1,47 @@
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 2 --zero_noise true --save_name 0.5_2_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 2 --zero_noise false --save_name 0.5_2_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 2 --zero_noise true --save_name 0.5_2_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 2 --zero_noise false --save_name 0.5_2_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 4 --zero_noise true --save_name 0.5_4_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 4 --zero_noise false --save_name 0.5_4_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 4 --zero_noise true --save_name 0.5_4_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 4 --zero_noise false --save_name 0.5_4_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 8 --zero_noise true --save_name 0.5_8_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 8 --zero_noise false --save_name 0.5_8_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 8 --zero_noise true --save_name 0.5_8_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 8 --zero_noise false --save_name 0.5_8_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 16 --zero_noise true --save_name 0.5_16_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 16 --zero_noise false --save_name 0.5_16_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 16 --zero_noise true --save_name 0.5_16_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 16 --zero_noise false --save_name 0.5_16_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 32 --zero_noise true --save_name 0.5_32_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 32 --zero_noise false --save_name 0.5_32_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 32 --zero_noise true --save_name 0.5_32_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 32 --zero_noise false --save_name 0.5_32_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 64 --zero_noise true --save_name 0.5_64_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 64 --zero_noise false --save_name 0.5_64_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 64 --zero_noise true --save_name 0.5_64_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 64 --zero_noise false --save_name 0.5_64_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 128 --zero_noise true --save_name 0.5_128_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 128 --zero_noise false --save_name 0.5_128_noise
-
-
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 128 --zero_noise true --save_name 0.5_128_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.5 --base_num_kernels 128 --zero_noise false --save_name 0.5_128_noise
 
 
 
 
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 2 --zero_noise true --save_name 0.758_2_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 2 --zero_noise false --save_name 0.758_2_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 4 --zero_noise true --save_name 0.758_4_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 4 --zero_noise false --save_name 0.758_4_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 8 --zero_noise true --save_name 0.758_8_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 8 --zero_noise false --save_name 0.758_8_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 2 --zero_noise true --save_name 0.758_2_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 2 --zero_noise false --save_name 0.758_2_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 16 --zero_noise true --save_name 0.758_16_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 16 --zero_noise false --save_name 0.758_16_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 4 --zero_noise true --save_name 0.758_4_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 4 --zero_noise false --save_name 0.758_4_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 32 --zero_noise true --save_name 0.758_32_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 32 --zero_noise false --save_name 0.758_32_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 8 --zero_noise true --save_name 0.758_8_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 8 --zero_noise false --save_name 0.758_8_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 64 --zero_noise true --save_name 0.758_64_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 64 --zero_noise false --save_name 0.758_64_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 16 --zero_noise true --save_name 0.758_16_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 16 --zero_noise false --save_name 0.758_16_noise
 
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 128 --zero_noise true --save_name 0.758_128_zero
-python -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 128 --zero_noise false --save_name 0.758_128_noise
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 32 --zero_noise true --save_name 0.758_32_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 32 --zero_noise false --save_name 0.758_32_noise
+
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 64 --zero_noise true --save_name 0.758_64_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 64 --zero_noise false --save_name 0.758_64_noise
+
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 128 --zero_noise true --save_name 0.758_128_zero
+python3 -u train_SinGAN.py --mode 3Dto2D --alpha_2 1 --alpha_6 0 --upsample_mode bilinear --spatial_downscale_ratio 0.758 --base_num_kernels 128 --zero_noise false --save_name 0.758_128_noise
